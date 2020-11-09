316f8fee00641e54cfdeee4e40716123d4208913
root <root@packer-Ubuntu-18.04-Server>
2020-11-09 21:04:55 +0100

    updated

--- model_training/trainmodel_fromfile_iterator.py
+++ model_training/trainmodel_fromfile_iterator.py
@@ -5,7 +5,7 @@ import re
 lettersanddotsonly = re.compile(r'[^a-zA-Z\.]')
 
 PATH = "../data/raw/embeddings/"
-outputpath='../data/'
+outputpath='/data/embedding_models/'
 FILENAME = "political_news_corpus.txt"
 
 #w2v_params = {
@@ -15,8 +15,8 @@ FILENAME = "political_news_corpus.txt"
 #}
 
 w2v_params = {
-    'size': 150,
-    'window': 10,
+    'size': 300,
+    'window': 5,
     'negative': 15
 }
 def preprocess(s):
@@ -36,7 +36,7 @@ class train_model():
         print('Estimated Word2Vec model')
 
 def train_and_save():
-    filename = f"{outputpath}w2v_size_150_window_10_negative_15"
+    filename = f"{outputpath}w2v_size_300_window_5_negative_15"
     casus = train_model()
 
     with open(filename, mode='wb') as fo:
